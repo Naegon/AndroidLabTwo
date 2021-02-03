54a6bb60b3bbceecb01811bf02eefe17da896ee5
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-02-03 19:58:50 +0100

    Step 19: Add ListActivity

--- app/src/main/java/com/test/flickrapp/MainActivity.java
+++ app/src/main/java/com/test/flickrapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.test.flickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnGetImg = findViewById(R.id.btnGetImg);
-        AppCompatImageView img = findViewById(R.id.image);
-        btnGetImg.setOnClickListener(new GetImageOnClickListener(img));
+        btnGetImg.setOnClickListener(new GetImageOnClickListener(findViewById(R.id.image)));
+
+        Button btnListActivity = findViewById(R.id.btnListActivity);
+        btnListActivity.setOnClickListener(v -> {
+            startActivity(new Intent(this, ListActivity.class));
+        });
     }
 }
